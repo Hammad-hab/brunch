5f4b69d8187ac74da8ce88bcd6cacfbd7edb4d89
CW team <hammadalibutt30@gmail.com>
2025-07-06 11:58:30 +0500

    fix: icns path

--- src/binfinder.py
+++ src/binfinder.py
@@ -7,15 +7,6 @@ import subprocess
 
 os.environ['DISPLAY'] = ':0'
 
-def format_path(path: str):
-    splits = path.split(' ')
-    if len(splits) <= 1:
-        return path # no need to format this, it has no spaces
-    
-    splits = [split.strip() + '\ ' for split in splits]
-    splits[-1] = splits[-1].replace('\ ', '').strip()
-    return r''.join(splits)
-
 dsv = ''
 
 if not os.path.isdir('/tmp/.xlunch'):
@@ -32,16 +23,16 @@ if sys.platform == 'darwin':
         reasources = (os.listdir(path))
         reasources = [reasource for reasource in reasources if '.icns' in reasource]
         icon = f'/Applications/{app}/Contents/Resources/{reasources[0]}' 
-        os.system(f'sips -s format png {icon} --out /tmp/.xlunch/{name}.png')
-        dsv += f'{name};/tmp/.xlunch/{name}.png;open /Applications/{format_path(app)}\n'
+        os.system(f'sips -s format png "{icon}" --out /tmp/.xlunch/{name.replace(" ", "_")}.png')
+        dsv += f'{name};/tmp/.xlunch/{name.replace(" ", "_")}.png;open /Applications/{app}\n'
 
-        MODE = '+x'
+    MODE = '+x'
 
-        if os.path.isfile('/tmp/.xlunch/entries.dsv'):
-            MODE = 'w'
+    if os.path.isfile('/tmp/.xlunch/entries.dsv'):
+        MODE = 'w'
 
-        with open('/tmp/.xlunch/entries.dsv', MODE) as f:
-            print('USING MODE', MODE)
-            f.write(dsv)
-        os.system('xquartz')
-        os.system('./xlunch --input /tmp/.xlunch/entries.dsv')
+    with open('/tmp/.xlunch/entries.dsv', MODE) as f:
+        print('USING MODE', MODE)
+        f.write(dsv)
+        
+    os.system('./xlunch --input /tmp/.xlunch/entries.dsv')
